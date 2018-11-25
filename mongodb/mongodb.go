7317c64fd3bc7cd98e45d702fe1de1950d2bdeed
David Marchbanks <ptdave20@gmail.com>
2018-11-25 18:48:11 -0500

    untest mongo middleware

--- mongodb/mongodb.go
+++ mongodb/mongodb.go
@@ -1,6 +1,9 @@
 package mongo
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"gopkg.in/mgo.v2"
 )
@@ -9,12 +12,13 @@ const ginMongoKey = "_mongo"
 
 //Config is used to initialize the middleware
 type Config struct {
-	Host     string `json:"host"`
-	UseAuth  bool   `json:"useAuth"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Database string `json:"database"`
-	Clone    bool   `json:"clone"`
+	Host        []string `json:"host"`
+	UseAuth     bool     `json:"useAuth"`
+	Username    string   `json:"username"`
+	Password    string   `json:"password"`
+	Database    string   `json:"database"`
+	Clone       bool     `json:"clone"` // Are we going to create a fresh instance or clone an existing one
+	FailOnIssue bool     `json:"failOnIssue"`
 }
 
 // GetMongo takes a context and returns the database or nil if it doesn't exist
@@ -28,7 +32,36 @@ func GetMongo(c *gin.Context) *mgo.Database {
 
 // InjectMongo takes a configuration and injects MGO into the your context
 func InjectMongo(config *Config) gin.HandlerFunc {
-	return func(c *gin.Context) {
+	var dInfo mgo.DialInfo
+	dInfo.Addrs = config.Host
+	dInfo.Database = config.Database
+	if config.UseAuth {
+		dInfo.Username = config.Username
+		dInfo.Password = config.Password
+	}
+	if config.Clone {
+		session, err := mgo.DialWithInfo(&dInfo)
+		if err != nil && config.FailOnIssue {
+			log.Fatalln(err)
+		}
+		return func(c *gin.Context) {
+			cSession := session.Clone()
 
+			c.Set(ginMongoKey, cSession.DB(dInfo.Database))
+			defer cSession.Close()
+			c.Next()
+		}
+	}
+
+	return func(c *gin.Context) {
+		session, err := mgo.DialWithInfo(&dInfo)
+		if err != nil && config.FailOnIssue {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		defer session.Close()
+		c.Set(ginMongoKey, session.DB(dInfo.Database))
+		c.Next()
 	}
 }
